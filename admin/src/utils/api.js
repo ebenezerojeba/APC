5c9db8aa1357fb48a10e6ad9b1923aed9a5e0ffc
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2026-02-26 20:36:33 -0330

    U

--- admin/src/utils/api.js
+++ admin/src/utils/api.js
@@ -95,7 +95,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://apcbackend.onrender.com/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://apcbackend.vercel.app//api';
 
 const api = axios.create({
   baseURL: BASE_URL,
