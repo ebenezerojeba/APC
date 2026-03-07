c26417449182dcad4e969af340048c078073a5c2
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2026-03-07 00:30:34 -0330

    U

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
+// import React from 'react'
+// import Hero from '../components/Hero'
+// import About from '../components/About'
+// import Vision from '../components/Vision'
+// import News from '../components/News'
+// import Event from '../components/Event'
+// import Contact from '../components/Contact'
+
+// const Home = () => {
+//   return (
+//     <div>
+//         <Hero />
+//         <About />
+//         <Vision />
+//         <News />
+//         <Event />
+//         <Contact />
+//     </div>
+//   )
+// }
+
+// export default Home
+
+
 import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Vision from '../components/Vision'
+import Leadership from '../components/Leadership'
 import News from '../components/News'
 import Event from '../components/Event'
 import Contact from '../components/Contact'
@@ -12,6 +37,7 @@ const Home = () => {
         <Hero />
         <About />
         <Vision />
+        <Leadership />
         <News />
         <Event />
         <Contact />
